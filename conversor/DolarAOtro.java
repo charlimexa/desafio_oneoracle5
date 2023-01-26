63f1c9d31de609f527da8d991957b4ab148c1960
Charli Campos Rodríguez <charli.charli180@gmail.com>
2023-01-26 04:51:51 -0600

    configurando dolar a otras monedas

---
+++ conversor/DolarAOtro.java
@@ -0,0 +1,9 @@
+System.out.println("1 Dollar = " + 79.37 + " Ruppe");
+    System.out.println();
+    System.out.println(amt+" Dollar = " + (amt*79.37) + " Ruppe");
+    System.out.println();
+
+    System.out.println("1 Dollar= " + 0.98 + " Euro");
+    System.out.println();
+
+    System.out.println(amt+" Dollar = " + (amt*0.98) + " Euro");
---
+++ conversor/EuroADolar.java
@@ -0,0 +1,13 @@
+package conversor;
+
+public class EuroADolar {
+    System.out.println("1 Dollar = " + 79.37 + " Ruppe");
+    System.out.println();
+    System.out.println(amt+" Dollar = " + (amt*79.37) + " Ruppe");
+    System.out.println();
+
+    System.out.println("1 Dollar= " + 0.98 + " Euro");
+    System.out.println();
+
+    System.out.println(amt+" Dollar = " + (amt*0.98) + " Euro");
+}
