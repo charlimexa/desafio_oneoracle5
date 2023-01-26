57d14550ba7d58385838fc3225b1c4116503a7fb
Charli Campos Rodríguez <charli.charli180@gmail.com>
2023-01-26 04:48:30 -0600

    configurando filtros

---
+++ rutinas/filtros.java
@@ -0,0 +1,42 @@
+package rutinas;
+
+public class filtros {
+    public static void main(String[] args) {
+        System.out.println("1 Ruppe");
+        System.out.println("2 Dollar");
+        System.out.println("3 Euro");
+        // take input
+        Scanner sc = new Scanner(System.in);
+        System. out.println("Choose the currency");
+        int choice = sc.nextInt();
+        System.out.println("Enter the amount");
+        double amount = sc.nextDouble();
+        // convirtiendo
+        switch (choice) {
+            case 1:
+                Ruppe_to_other(amount);
+                break;
+            case 2:
+                Dollar_to_other(amount);
+                break;
+            case 3:
+                Euro_to_other(amount);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+    }
+
+public static void Ruppe_to_other(double amt) {
+    System.out.println("1 Ruppe = " + 0.013 + " Dollar");
+    System.out.println();
+
+    System.out.println(amt+" Ruppe = " + (amt*0.013) + " Dollar");
+    System.out.println();
+
+    System.out.println("1 Ruppe = " + 0.012 + " Euro");
+    System.out.println();
+    System.out.println(amt+" Ruppe = " + (amt*0.012) + " Euro");
+    System.out.println();
+}
